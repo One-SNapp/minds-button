b22a1a49f32efac1e67072ed56265ff3536f00eb
One-SNapp <one.snapp8@gmail.com>
2016-07-04 23:13:37 -0700

    Update button.js

--- button.js
+++ button.js
@@ -1,5 +1,5 @@
-var sharedContent = sharedURL;
 var sharedURL = "http://www.example.com/";
+var sharedContent = sharedURL;
 function getUrl() {
 window.location.href= sharedURL;
 createCookie();
@@ -7,4 +7,3 @@ createCookie();
 function createCookie() {
 document.cookie = "username="+sharedContent+"; expires=Wed, 1 Jan 2025 13:47:11 UTC; path=/"
 }
-
